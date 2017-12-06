e9bb312018864c285067082bf6ea2a08a36fcd7c
Raphael Jacob <r.jacob2002@gmail.com>
2017-12-06 17:05:06 +0100

    open shopping list by name

--- BringPy/high_level.py
+++ BringPy/high_level.py
@@ -11,3 +11,12 @@ class BringPy(Bring):
     def __init__(self):
         Bring.__init__(self)
 
+    def openShoppingList(self, name):
+        index = -1
+        for i, l in enumerate(self.getShoppingLists()):
+            if l['name'] == name:
+                index = i
+                break
+        if index == -1:
+            raise KeyError
+        Bring.openShoppingList(self, index)
